fed3323c7257948c31d060d4a3caf56efe9e06f7
abdul kadar khan <monukadar12345@gmail.com>
2024-12-23 18:05:18 +0530

    stage3

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,8 +3,10 @@ import "./card.css"
 
 const Card = ({card}) => { 
   return (
-    <div className={`card ${card}`}> 
-      Card
+    <div className={`card ${card} floating`}> 
+      <h2>GLASS MORPHISM</h2>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum minima tenetur animi, tempore ducimus exercitat.</p>
+      <button className='btnCard'>Button</button> 
     </div>
   )
 }
