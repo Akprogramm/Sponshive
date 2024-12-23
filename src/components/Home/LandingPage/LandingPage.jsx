964ebee7ceffe4f43ad3788e58b5bbc34af3236f
abdul kadar khan <monukadar12345@gmail.com>
2024-12-23 13:24:44 +0530

    message

--- src/components/Home/LandingPage/LandingPage.jsx
+++ src/components/Home/LandingPage/LandingPage.jsx
@@ -3,16 +3,16 @@ import './landingpage.css'
 import Navbar from "../../Navbar/Navbar"
 
 const LandingPage = () => {
-  // const [colr,setColr] = useState(false); 
+  // const [hexagonGreatGrandChild,setHexagonGreatGrandChild] = useState(false); 
   return (
     <div className='LandingPage' >
-      <div className="hexagonP" id="hexagonP" >
+      <div className="hexagonP" id="hexagonP" > 
           <div className="hexagon" id="hexagon" >
             <div className="hexagonChild" id="hexagonChild" >
               <div className="hexagonGrandChild" id="hexagonGrandChild">
-              <div  className="hexagonGreatGrandChild" id="hexagonGreatGrandChild" >
-                <div className="work" >Sponshive</div>
-              </div>
+              <div  className="hexagonGreatGrandChild" id="hexagonGreatGrandChild">
+                <div className="work" >SPONSHIVE</div>
+              </div> 
               </div>
             </div>
           </div>
